b15a30719c2a286c46e0a12bab65e8b3f0fc4371
souleymaneESP <18301@esp.mr>
2019-12-26 00:09:10 +0100

    detCaseVide Update

--- detection d'une case vide/detCaseVide.py
+++ detection d'une case vide/detCaseVide.py
@@ -1,6 +1,16 @@
 import cv2
 import numpy as np
+import pytesseract
 
+#Faire les commandes suivantes pour installer pytesseract
+
+#"pip3 install pytesseract" pour python 3 et pip au lieu de pip3 pour python2
+
+#sudo apt update
+#sudo apt install tesseract-ocr
+#sudo apt install libtesseract-dev
+
+f=open("listeNum.txt","a+")
 def detect_case(img):
     img2 = cv2.imread(img)     
 
@@ -14,10 +24,14 @@ def detect_case(img):
    #Les numeros 65,95,35,65 sont les coordonnées des 4 pixels sommets du rectangle qui doit etre vide pour les personnes qui n'ont pas voté 
 
     if n>850:
-        print("Il n'a pas voté")
+        print("Il n'a pas voté et son Numero a été ajouté au fichier texte listeNum.txt")
+        f.write(pytesseract.image_to_string(img2)[0:11]+"--------------------------" +"\n")        
+
+
         return 0
+        
     else :
-        print("Il a bien voté")
+        print("Il a pas voté")
         return 1
 
-detect_case("41.png")
+detect_case("2.png")
