5df26c952508fe440bbddc85b8e8b59138ad0047
moulay03 <49500089+moulay03@users.noreply.github.com>
2020-01-25 21:20:08 +0000

    Delete exectxt.py

--- exectxt.py
+++
@@ -1,9 +0,0 @@
-import sys
-
-orig = sys.stdout
-with open("output.txt", "wb") as f:
-    sys.stdout = f
-    try:
-        execfile("le_fichier_py_à_exécuter", {})
-    finally:
-        sys.stdout = orig
